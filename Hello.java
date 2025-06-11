4192ae255fce3f3f8059bab494c69a87e1ba6870
Amal Hammoud <amalhamm@umich.edu>
2025-06-11 14:04:49 -0400

    update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,9 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
-<<<<<<< HEAD
-        System.out.println("Hello from the new branch!");
-=======
-        System.out.println("Hello, world!");
->>>>>>> ebde57ee14d0b1ea958ed6936e8064c4519ccb7c
+        for(int i=1; i<=10; i++) {
+            System.out.println("Hello World..."+i);
+        }
     }
 }
